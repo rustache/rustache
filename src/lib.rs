091fab2023e641afd84411c48d045d5c75c38cb7
Sean Chen <skypemaster007@gmail.com>
2014-09-19 16:25:10 -0700

    Small edits to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,10 @@ extern crate regex;
 use std::io::{File, MemWriter, stdout};
 
 // Helpers
-
-// Retrieves a file from disk and converts to string. Returns the template string.
-fn get_template(template_path: &str) -> Result<String, int> {
+fn get_template(template_path: &str) -> String {
     let path = Path::new(template_path);
     let display = path.display();
 
-    // open the file
     let mut file = match File::open(&path) {
         Err(why) => fail!("Couldn't open {}: {}", display, why.desc),
         Ok(file) => file,
@@ -43,7 +40,6 @@ fn render_template_with_data<W: Writer>(writer: &mut W, data: &str) {
 
 #[test]
 fn should_retrieve_file() {
-
     let path = "src/test_templates/sample.html";
     let expected = String::from_str("<div>");
     let retrieved_template = get_template(path);
@@ -53,29 +49,4 @@ fn should_retrieve_file() {
     // render_template_with_data(&stream, passed_template);
 
     assert_eq!(retrieved_template, Ok(expected));
-}
-
-
-// Capture all regex matches for rustache tags and return them as a vector of
-// string slices.  Results will be used by the parser in order to create the
-// TagMap.
-fn find_tag_matches(input: &str) -> Vec<&str>{
-    let mut result: Vec<&str> = Vec::new();
-    let re = regex!(r"(\{\{\s?[\w\s]*\s?\}\})");
-    
-    for cap in re.captures_iter(input) {
-        result.push(cap.at(1));
-    }
-
-    result
-}
-
-
-
-#[test]
-fn test_bucketing() {
-    let test_string: &str = "{{variable1}},{{variable2}},{{variable3}}";
-    let expected: Vec<&str> = vec!["{{variable1}}","{{variable2}}","{{variable3}}"];
-    let result = find_tag_matches(test_string);
-    assert_eq!(result, expected);
 }
