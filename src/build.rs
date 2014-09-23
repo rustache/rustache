0e4f484ced535c3be1ae4c5298a86a27eae48427
Sean Chen <skypemaster007@gmail.com>
2014-09-23 16:43:11 -0700

    Renamed create_data_map to normalize_data_map

--- src/build.rs
+++ src/build.rs
@@ -13,28 +13,24 @@ impl<'a> Builder<'a> {
         }
     }
 
-    pub fn insert_static<K: StrAllocating, V: StrAllocating>(self, key: K, value: V) -> Builder<'a> {
-        let Builder { mut data } = self;
-        data.insert(key.into_string(), Static(value.into_string()));
-        Builder { data: data }
-    }
-
-    pub fn insert_bool<K: StrAllocating>(self, key: K, value: bool) -> Builder<'a> {
-        let Builder { mut data } = self;
-        data.insert(key.into_string(), Bool(value));
-        Builder { data: data }
-    }
-
-    pub fn create_data_map<'a>(tags: HashSet<String>, data: HashMap<&'a str, &'a str>) -> HashMap<String, String> {
+    pub fn normalize_data_map<'a>(tags: HashMap<&'a str, Data<'a>>, data: HashMap<&'a str, Data<'a>>) -> HashMap<&'a str, Data<'a>> {
         let mut value_map = HashMap::new();
 
         for tag in tags.into_iter() {
-            let datum = data.find_equiv(&tag.as_slice())
-                .unwrap_or(&"")
-                .to_string();
-            value_map.insert(tag, datum);
+            let datum = data.find_equiv(&tag.as_slice())  // -> Option<'a V>
+            // if data.find_equiv() finds Some, we'll want to perform a match on the returned Data
+            // if data.find_equiv() finds None, return a default String
+                            .unwrap_or(&Static("".to_string()))
+                            .to_string();
+            match datum {
+                Static => ,
+                Bool   => ,
+                Vector => ,
+                Map    =>
+            }
         }
 
         value_map
     }
+
 }
