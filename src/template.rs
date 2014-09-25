364e15132287490f5c41c2ba471b0589be02673a
Fleur Dragan <fleur@obscure.org>
2014-09-25 16:49:45 -0700

    removed unused variable from function prototypes in template.rs

--- src/template.rs
+++ src/template.rs
@@ -1,8 +1,6 @@
-use std::collections::hashmap::HashMap;
-use parser::{Parser, Node, Value, Static, Unescaped};
+use parser::{Parser, Value, Static, Unescaped};
 use build::{HashBuilder};
 use super::{Data, Str, Bool, Vector, Hash};
-use std::io::stdio::stdout;
 pub struct Template<'a>;
 
 impl<'a> Template<'a> {
@@ -24,7 +22,7 @@ impl<'a> Template<'a> {
         rv
     }
 
-    fn handle_unescaped_node<'a, W: Writer>(key: &String, data: &Data, writer: &mut W) {
+    fn handle_unescaped_node<'a, W: Writer>(data: &Data, writer: &mut W) {
         let mut tmp: String = String::new();
         match *data {
 
@@ -51,7 +49,7 @@ impl<'a> Template<'a> {
         }        
     }
 
-    fn handle_value_node<'a, W: Writer>(key: &String, data: &Data, writer: &mut W) {
+    fn handle_value_node<'a, W: Writer>(data: &Data, writer: &mut W) {
         let mut tmp: String = String::new();
         match *data {
 
@@ -89,14 +87,14 @@ impl<'a> Template<'a> {
                     let tmp = key.to_string();
                     if datastore.data.contains_key(&tmp) {
                         let ref val = datastore.data[tmp];
-                        Template::handle_unescaped_node(&tmp, val, writer);
+                        Template::handle_unescaped_node(val, writer);
                     }
                 }
                 Value(key) => {
                     let tmp = key.to_string();
                     if datastore.data.contains_key(&tmp) {
                         let ref val = datastore.data[tmp];
-                        Template::handle_value_node(&tmp, val, writer);
+                        Template::handle_value_node(val, writer);
                     }
                 }
 
@@ -114,26 +112,24 @@ impl<'a> Template<'a> {
 
 #[cfg(test)]
 mod template_tests {
-     use std::collections::hashmap::HashMap;
-     use std::io::MemWriter;
+    use std::io::MemWriter;
+    use std::str;
 
-     use parser::Parser;
-     use template::Template;
-     use compiler::Compiler;
-     use build::HashBuilder;
-     use std::str;
+    use parser::Parser;
+    use template::Template;
+    use compiler::Compiler;
+    use build::HashBuilder;
 
     #[test]
     fn test_escape_html() {
-        let mut data_map: HashMap<String, String> = HashMap::new();
         let s1 = "a < b > c & d \"spam\"\'";
         let a1 = "a &lt; b &gt; c &amp; d &quot;spam&quot;'";
         let s2 = "1<2 <b>hello</b>";
         let a2 = "1&lt;2 &lt;b&gt;hello&lt;/b&gt;";
 
         let mut w = MemWriter::new();
-        let mut compiler = Compiler::new("{{ value }}");
-        let mut parser = Parser::new(&compiler.tokens);
+        let compiler = Compiler::new("{{ value }}");
+        let parser = Parser::new(&compiler.tokens);
         let mut data = HashBuilder::new().insert_string("value", s1);
         Template::render_data(&mut w, &data, &parser);
         assert_eq!(a1, str::from_utf8(w.get_ref()).unwrap());
@@ -142,19 +138,24 @@ mod template_tests {
         data = HashBuilder::new().insert_string("value", s2);
         Template::render_data(&mut w, &data, &parser);
         assert_eq!(a2, str::from_utf8(w.get_ref()).unwrap());
+    }
 
-        w = MemWriter::new();
+    #[test]
+    fn test_not_escape_html() {
+        let s2 = "1<2 <b>hello</b>";
+        let mut w = MemWriter::new();
         let compiler2 = Compiler::new("{{& value }}");
-        parser = Parser::new(&compiler2.tokens);
-        data = HashBuilder::new().insert_string("value", s2);
+
+        let parser = Parser::new(&compiler2.tokens);
+        let data = HashBuilder::new().insert_string("value", s2);
+
         Template::render_data(&mut w, &data, &parser);
-        assert_eq!(s2, str::from_utf8(w.get_ref()).unwrap());
+        assert_eq!(s2, str::from_utf8(w.get_ref()).unwrap());        
     }
 
     #[test]
     fn test_render_to_io_stream() {
         let mut w = MemWriter::new();
-        let mut data_map: HashMap<String, String> = HashMap::new();
 
         let data = HashBuilder::new().insert_string("value1", "The heading");
 
