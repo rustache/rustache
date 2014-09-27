65a01cee4390a376484d3e1ee192075918201ab1
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2014-09-26 21:36:48 -0700

    (test) added end to end test, updated gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,56 @@ use parser::{Parser};
 
 pub use build::{HashBuilder, VecBuilder};
 
-pub fn render<'a, W: Writer>(path: Path, data: &HashBuilder, writer: &mut W) {
-    let file = Read::read_file(path);
-    let compiler = Compiler::new(file.as_slice());
-    let parser = Parser::new(&compiler.tokens);
-    Template::new().render_data(writer, data, &parser);
+#[cfg(test)]
+mod lib_tests {
+    use std::io::MemWriter;
+    use std::io::File;
+
+    use build::HashBuilder;
+    use super::Rustache;
+
+    #[test]
+    fn file_end_to_end_test() {
+        let mut w = MemWriter::new();
+        let path = Path::new("test_data/index.template.html");
+        let data = HashBuilder::new()
+            .insert_hash("people", |builder| {
+                builder.insert_vector("information", |builder| {
+                    builder
+                        .push_string("<tr><td>Jarrod</td><td>Ruhland</td></tr>")
+                        .push_string("<tr><td>Sean</td><td>Chen</td></tr>")
+                        .push_string("<tr><td>Fleur</td><td>Dragan</td></tr>")
+                        .push_string("<tr><td>Jim</td><td>O'Brien</td></tr>")
+                    }
+                )}
+            );
+        Rustache::render(path, &data, &mut w);
+
+        let mut f = File::create(&Path::new("test_data/index.html"));
+        let completed = f.write(w.unwrap().as_slice());
+        assert_eq!(completed, Ok(()));
+    }
 }
 
-pub fn render_text<'a, W: Writer>(input: &'a str, data: &HashBuilder, writer: &mut W) {
-    let compiler = Compiler::new(input);
-    let parser = Parser::new(&compiler.tokens);
-    Template::new().render_data(writer, data, &parser);
+pub struct Rustache;
+
+impl Rustache {
+    pub fn new () -> Rustache{
+        Rustache
+    }
+
+    pub fn render<'a, W: Writer>(path: Path, data: &HashBuilder, writer: &mut W) {
+        let file = Read::read_file(path);
+        let compiler = Compiler::new(file.as_slice());
+        let parser = Parser::new(&compiler.tokens);
+        Template::new().render_data(writer, data, &parser);
+    }
+
+    pub fn render_text<'a, W: Writer>(input: &'a str, data: &HashBuilder, writer: &mut W) {
+        let compiler = Compiler::new(input);
+        let parser = Parser::new(&compiler.tokens);
+        Template::new().render_data(writer, data, &parser);
+    }
 }
 
 pub struct Read;
